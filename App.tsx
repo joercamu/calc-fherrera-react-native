004510fa29f63bf1096fee3d373aed638bf41c85
Jonathan Cardenas, PC Asus <jonathan.cardenasm29@gmail.com>
2022-03-26 10:23:32 -0500

    fundamentos

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { CounterScreen } from './src/screens/CounterScreen';
+import {CounterScreen} from './src/screens/CounterScreen';
 // import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
+import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
+import {SafeAreaView} from 'react-native';
+import { DimensionsScreen } from './src/screens/DimensionsScreen';
+import { PositionScreen } from './src/screens/PositionScreen';
 
 export const App = () => {
   return (
-    // <HelloWorldScreen />
-    <CounterScreen/>
+    <SafeAreaView style={{flex: 1}}>
+      {/* <HelloWorldScreen /> */}
+      {/* <CounterScreen /> */}
+      {/* <BoxObjectModelScreen /> */}
+      {/* <DimensionsScreen/> */}
+      <PositionScreen/>
+    </SafeAreaView>
   );
 };
