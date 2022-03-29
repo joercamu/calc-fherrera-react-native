1ef6e98699ad8d853de55d67c54c990ad1a090de
Jonathan Cardenas, PC Asus <jonathan.cardenasm29@gmail.com>
2022-03-28 21:32:15 -0500

    set enveiroment calculator app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-// import {CounterScreen} from './src/screens/CounterScreen';
-// // import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
-// import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
-// import { DimensionsScreen } from './src/screens/DimensionsScreen';
-// import { PositionScreen } from './src/screens/PositionScreen';
-// import {FlexScreen} from './src/screens/FlexScreen';
-import { TaskScrren } from './src/screens/TaskScrren';
+import { CalculatorScreen } from './src/screens/CalculatorScreen';
+
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      {/* <HelloWorldScreen /> */}
-      {/* <CounterScreen /> */}
-      {/* <BoxObjectModelScreen /> */}
-      {/* <DimensionsScreen/> */}
-      {/* <PositionScreen/> */}
-      {/* <FlexScreen /> */}
-      <TaskScrren/>
+        <CalculatorScreen/>
     </SafeAreaView>
   );
 };
